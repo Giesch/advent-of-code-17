7a4753a548a2d146badf4310b324f25d1cbd6b0b
Dan Knutson <dan.knutson@gmail.com>
2017-12-30 10:21:22 -0600

    day 3 part 2

--- src/advent_of_code/day_3.clj
+++ src/advent_of_code/day_3.clj
@@ -1,4 +1,5 @@
-(ns advent-of-code.day-3)
+(ns advent-of-code.day-3
+  (:require [advent-of-code.core :refer :all]))
 
 ;;;; Part 1
 
@@ -22,3 +23,65 @@
   (last (take n distances)))
 
 ;;;; Part 2
+
+(def offsets
+  {:north [0 1]
+   :east  [1 0]
+   :south [0 -1]
+   :west  [-1 0]})
+
+(def to-the-left
+  {:north :west
+   :east  :north
+   :south :east
+   :west  :south})
+
+(def initial-state
+  {:grid {[0 0] 1},
+   :facing :south,
+   :location [0 0]})
+
+(defn add-pairs [[x1 y1] [x2 y2]]
+  [(+ x1 x2) (+ y1 y2)])
+
+(defn look-dir [loc dir]
+  (add-pairs loc (dir offsets)))
+
+(defn look-left
+  [{:keys [grid facing location]}]
+  (get grid
+       (look-dir location (facing to-the-left))
+       nil))
+
+(defn choose-direction
+  [{:keys [facing] :as state}]
+  (if (look-left state)
+    facing
+    (facing to-the-left)))
+
+(defn neighbors [loc]
+  (map (partial add-pairs loc)
+       (for [x [-1 0 1]
+             y [-1 0 1]
+             :when (not (and (zero? x) (zero? y)))]
+         [x y])))
+
+(defn sum-neighbors [grid loc]
+  (apply + (map #(get grid % 0)
+                (neighbors loc))))
+
+(defn step
+  [{:keys [grid facing location] :as state}]
+  (let [dir      (choose-direction state)
+        next-loc (look-dir location dir)
+        v        (sum-neighbors grid next-loc)]
+    {:grid     (assoc grid next-loc v),
+     :facing   dir,
+     :location next-loc}))
+
+(defn lookup [{:keys [grid location]}]
+  (get grid location))
+
+(defn advent-3-2 [n]
+  (lookup (find-first #(< n (lookup %))
+                      (iterate step initial-state))))
