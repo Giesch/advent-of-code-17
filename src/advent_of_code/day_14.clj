0d4a00471df843fee7c3235d3cd14a6c7c5757e6
Dan Knutson <dan.knutson@gmail.com>
2017-12-29 21:15:08 -0600

    day 14 part 2

--- src/advent_of_code/day_14.clj
+++ src/advent_of_code/day_14.clj
@@ -1,5 +1,8 @@
 (ns advent-of-code.day-14
-  (:require [advent-of-code.day-10 :refer [advent-10-2]]))
+  (:require [advent-of-code.day-10 :refer [advent-10-2]]
+            [advent-of-code.core :refer :all]))
+
+;;;; Part 1
 
 (def hex-ch->bin-vec
   {\0 [0 0 0 0],
@@ -26,21 +29,62 @@
   (map advent-10-2 (row-keys s)))
 
 (defn- row-hash->bin-vec [s]
-  (apply concat (map hex-ch->bin-vec s)))
+  (mapcat hex-ch->bin-vec s))
 
 (defn- all-squares [key-str]
-  (map row-hash->bin-vec
-       (row-hashes key-str)))
+  (->> key-str
+       (row-hashes)
+       (map row-hash->bin-vec)
+       (map vec)
+       (vec)))
 
 (defn advent-14-1 [s]
   (reduce #(apply + %1 %2)
           0
           (all-squares s)))
 
-(comment
+;;;; Part 2
 
-  (def squares (all-squares "jxqlasbh"))
+(defn neighbor-coordinates [row col]
+  (apply clojure.set/union
+         (for [r [(dec row) (inc row)]
+               c [(dec col) (inc col)]
+               :when (and (<= 0 r 127)
+                          (<= 0 c 127))]
+           #{[row c] [r col]})))
 
-  (advent-14-1 "jxqlasbh")
+(defn lookup [squares row col]
+  (if-let [r (nth squares row nil)]
+    (nth r col nil)
+    nil))
 
-  )
+(defn squash-group [squares row col]
+  (let [squares   (assoc-in squares [row col] 0)
+        neighbors (neighbor-coordinates row col)]
+    (reduce (fn [sqs [r c]] (squash-group sqs r c))
+            squares
+            (filter (fn [[r c]] (= 1 (lookup squares r c)))
+                    neighbors))))
+
+(defn first-one [row]
+  (first (find-first #(= 1 (second %))
+                     (indexed row))))
+
+(defn one-coord [squares]
+  (reduce (fn [_ [r row]]
+            (if-let [c (first-one row)]
+              (reduced [r c])
+              nil))
+          nil
+          (indexed squares)))
+
+(defn count-groups [squares]
+  (loop [squares squares
+         groups  0]
+    (if-let [[r c] (one-coord squares)]
+      (recur (squash-group squares r c)
+             (inc groups))
+      groups)))
+
+(defn advent-14-2 [s]
+  (count-groups (all-squares s)))
