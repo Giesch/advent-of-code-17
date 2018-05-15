5615dcaaf223d4c2ff9ab0120f039057729caa5a
Dan Knutson <dan.knutson@gmail.com>
2018-05-15 00:09:00 -0500

    borrow checker let me rotate a matrix

--- rust/day_21/src/main.rs
+++ rust/day_21/src/main.rs
@@ -1,10 +1,15 @@
 use std::fs;
+use std::collections::HashSet;
 
 fn main() {
     // let start_pattern = parse_pixels(".#./..#/###");
     // println!("{:#?}", start_pattern);
     let rules = read_input("input.txt");
-    println!("{:#?}", rules);
+
+    // let matrix = &rules[50].before;
+    // println!("{:#?}", matrix);
+    // let rotated = rotate(matrix);
+    // println!("{:#?}", rotated);
 }
 
 fn read_input(filename: &str) -> Vec<Rule> {
@@ -17,7 +22,7 @@ fn read_input(filename: &str) -> Vec<Rule> {
 
 type Pixels = Vec<Vec<Pixel>>;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 enum Pixel {
     On,
     Off,
@@ -44,19 +49,102 @@ fn parse_pixels(line: &str) -> Pixels {
 struct Rule {
     before: Pixels,
     after: Pixels,
+    matches: HashSet<Pixels>,
 }
 
-// fn rotate(vs: Vec<Vec<Pixel>>) -> Vec<Vec<Pixel>> {
-//     for row in vs {
-//     }
-// }
+trait Rotatable {
+    fn rotate_right(&self) -> Self;
+    fn rotate_left(&self) -> Self;
+    fn rotate_180(&self) -> Self;
+}
+
+impl Rotatable for Pixels {
+    fn rotate_right(&self) -> Pixels {
+        let size = self.len();
+        let mut rotated = vec![vec![Pixel::Off; size]; size];
+        for _row in 0..size {
+            let col = Vec::with_capacity(size);
+            rotated.push(col);
+        }
+
+        for row in 0..size {
+            for col in 0..size {
+                rotated[row][col] = self[size - col - 1][row];
+            }
+        }
+
+        rotated
+    }
+
+    fn rotate_left(&self) -> Pixels {
+        let size = self.len();
+        let mut rotated = vec![vec![Pixel::Off; size]; size];
+        for _row in 0..size {
+            let col = Vec::with_capacity(size);
+            rotated.push(col);
+        }
+
+        for row in 0..size {
+            for col in 0..size {
+                rotated[row][col] = self[row][size - col - 1];
+            }
+        }
+
+        rotated
+    }
+
+    fn rotate_180(&self) -> Pixels {
+        self.iter().cloned().rev().collect()
+    }
+}
+
+fn build_rule_match_set(pixels: &Pixels) -> HashSet<Pixels> {
+    let mut matches: HashSet<Pixels> = HashSet::new();
+
+    // let pixelses = [
+    //     pixels.clone(),
+
+    //     pixels.rotate_right(),
+    //     pixels.rotate_right().rotate_right(),
+    //     pixels.rotate_right().rotate_right().rotate_right(),
+
+    //     // pixels.rotate_right(),
+    //     // pixels.rotate_left(),
+    //     // pixels.rotate_180(),
+
+    // ];
+
+    // for p in pixelses.into_iter() {
+    //     matches.insert(*p);
+    // }
+
+    // this can't be what the borrow checker wanted
+    matches.insert(pixels.clone());
+    matches.insert(pixels.rotate_right());
+    matches.insert(pixels.rotate_left());
+    matches.insert(pixels.rotate_180());
+
+    // let rotated = pixels.rotate();
+    // matches.insert(rotated);
+    // let rotated = rotated.rotate();
+    // matches.insert(rotated);
+    // let rotated = rotated.rotate();
+    // matches.insert(rotated);
+
+    // matches.insert(pixels);
+
+    matches
+}
 
 impl Rule {
     fn from_string(line: &str) -> Rule {
         let tokens: Vec<&str> = line.split(" => ").collect();
+        let before = parse_pixels(tokens[0]);
+        let matches = build_rule_match_set(&before);
 
         Rule {
-            before: parse_pixels(tokens[0]),
+            before,
+            matches,
             after: parse_pixels(tokens[1]),
         }
     }
