e89a283b976088fe6f927c6db510bdd3e524f616
Dan Knutson <dan.knutson@gmail.com>
2018-07-14 13:20:04 -0500

    day 22 part 2

--- clojure/src/advent_of_code/day_22.clj
+++ clojure/src/advent_of_code/day_22.clj
@@ -31,6 +31,13 @@
     :left  :up
     :right :down))
 
+(defn reverse-direction [dir]
+  (case dir
+    :up    :down
+    :down  :up
+    :left  :right
+    :right :left))
+
 (defn change-facing
   [{:keys [infected-nodes facing current-node] :as state}]
   (assoc state :facing (if (infected-nodes current-node)
@@ -44,9 +51,9 @@
         update-fn      (if infected? disj conj)
         inc-infections (not infected?)]
     (-> (if inc-infections
-         (update state :infections-caused inc)
-         state)
-       (update :infected-nodes #(update-fn % coord)))))
+          (update state :infections-caused inc)
+          state)
+        (update :infected-nodes #(update-fn % coord)))))
 
 (defn move-forward [state]
   (let [dir ((:facing state) directions)]
@@ -54,10 +61,10 @@
 
 (defn advance [state]
   (-> state
-     change-facing
-     swap-infection
-     move-forward
-     (update :bursts-executed inc)))
+      change-facing
+      swap-infection
+      move-forward
+      (update :bursts-executed inc)))
 
 (defn parse-infected-nodes [input]
   (let [lines       (string/split-lines input)
@@ -75,15 +82,71 @@
                 (map (fn [[position infected]] position)))
           lines)))
 
-(defn parse-input [input]
-  (assoc (new-state) :infected-nodes (parse-infected-nodes input)))
+(defn parse-input [input nodes-parser]
+  (assoc (new-state) :infected-nodes (nodes-parser input)))
 
 (defn solve [input n]
-  (last (take (inc n) (iterate advance (parse-input input)))))
+  (last (take (inc n) (iterate advance (parse-input input parse-infected-nodes)))))
 
+(defn part-2-state []
+  {:current-node      [0 0]
+   :facing            :up
+   :node-states       (hash-map) ;; :clean :infected :weakened :flagged
+   :infections-caused 0
+   :bursts-executed   0})
+
+(defn parse-node-states [input]
+  (let [lines       (string/split-lines input)
+        line->bools (fn [line] (vec (map #(= % \#) line)))
+        side-len    (count lines) ;; assuming it's a square
+        shift       (quot side-len 2)
+        two-d-ify   (fn [n infected] [[(- (mod n side-len) shift)
+                                       (- (- (quot n side-len) shift))]
+                                      infected])]
+    (into (hash-map)
+          (comp (map string/trim)
+                (mapcat line->bools)
+                (map (fn [b] (if b :infected :clean)))
+                (map-indexed two-d-ify))
+          lines)))
+
+(defn parse-part-2 [input]
+  (assoc (part-2-state) :node-states (parse-node-states input)))
+
+(defn part-2-change-facing [{:keys [current-node node-states facing] :as state}]
+  (assoc state :facing (case (get node-states current-node :clean)
+                         :clean    (turn-left facing)
+                         :infected (turn-right facing)
+                         :weakened facing
+                         :flagged (reverse-direction facing))))
+
+(defn part-2-modify-node [{:keys [current-node] :as state}]
+  (let [increment-infections (fn [state] (if (= :infected (get-in state [:node-states current-node]))
+                                           (update state :infections-caused inc)
+                                           state))]
+    (-> state
+        (update-in [:node-states current-node]
+                   #(case %
+                      :clean :weakened
+                      :infected :flagged
+                      :weakened :infected
+                      :flagged :clean
+                      :weakened))
+        increment-infections)))
+
+(defn part-2-advance [state]
+  (-> state
+      part-2-change-facing
+      part-2-modify-node
+      move-forward
+      (update :bursts-executed inc)))
+
+(defn solve-part-2 [input n]
+  (last (take (inc n) (iterate part-2-advance (parse-part-2 input)))))
 
 (comment
-  (parse-input input)
+  (parse-input input parse-infected-nodes)
+  (parse-part-2 input)
 
   (def example-string
     "..#
@@ -91,18 +154,22 @@
   ...")
 
   (def example-input
-    (parse-input example-string))
+    (parse-input example-string parse-infected-nodes))
 
   (advance
    (advance example-input))
   (swap-infection
    (change-facing example-input))
 
+  (part-2-advance (parse-part-2 example-string))
+
+  (solve-part-2 example-string 100)
+  (solve-part-2 example-string 10000000)
+  (solve-part-2 input 10000000)
+
   (solve example-string 10000)
 
-  (solve input 10000)
-
-  )
+  (solve input 10000))
 
 (def input
   "#.###...#..#..#...##.####
